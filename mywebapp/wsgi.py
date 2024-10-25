745af5791920eca5fc4d1b04f3e2679216fbc223
User <m.tzima@theprosperty.com>
2024-10-25 22:18:28 +0300

    Fix project structure and settings

--- mywebapp/wsgi.py
+++ mywebapp/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ppgis.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mywebapp.settings')
 
 application = get_wsgi_application()
